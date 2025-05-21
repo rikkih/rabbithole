f177eb82b28f55cfaf6af967ffe4a4919eb6049f
Hoderick <rikkih@protonmail.com>
2025-05-21 19:55:15 +0100

    update Message interface

--- src/main/java/com/hoderick/rabbithole/chat/Message.java
+++ src/main/java/com/hoderick/rabbithole/chat/Message.java
@@ -8,4 +8,8 @@ import lombok.Setter;
 public class Message {
     private String from;
     private String text;
+    private String timestamp;
+    // TODO: This should probably be picked up from the DB. Check if this is worthwhile. We'll need to do a GET for each
+    // websocket send. Perhaps we can just use the chat page to render the user image.
+    private String avatarUrl;
 }
