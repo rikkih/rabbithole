849d3b39c9afee1bfb96723bdecd03189b72b729
Hoderick <rikkih@protonmail.com>
2025-05-22 19:58:31 +0100

    Generate presigned urls for user avatars

--- src/main/java/com/hoderick/rabbithole/s3/S3StorageServiceImpl.java
+++ src/main/java/com/hoderick/rabbithole/s3/S3StorageServiceImpl.java
@@ -7,18 +7,46 @@ import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
 import software.amazon.awssdk.services.s3.model.NoSuchBucketException;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class S3StorageServiceImpl implements S3StorageService {
 
     private final S3Client s3Client;
+    private final S3Presigner s3Presigner;
+
+    @Override
+    public void delete(String bucket, String key) {
+        s3Client.deleteObject(DeleteObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build());
+    }
+
+    @Override
+    public URL generatePresignedUrl(String bucket, String key) {
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build();
+
+        GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(60))
+                .getObjectRequest(getObjectRequest)
+                .build();
+
+        return s3Presigner.presignGetObject(presignRequest).url();
+    }
 
     @Override
     public void upload(String bucket, String key, MultipartFile file) {
@@ -33,20 +61,6 @@ public class S3StorageServiceImpl implements S3StorageService {
         } catch (IOException ioe) {
             throw new RuntimeException("Failed to upload Avatar.", ioe);
         }
-    }
-
-    @Override
-    public void delete(String bucket, String key) {
-        s3Client.deleteObject(DeleteObjectRequest.builder()
-                .bucket(bucket)
-                .key(key)
-                .build());
-    }
-
-    @Override
-    public URL generatePresignedUrl(String bucket, String key) {
-        return s3Client.utilities()
-                .getUrl(builder -> builder.bucket(bucket).key(key));
     }
 
     private void ensureBucketExists(String bucket) {
--- src/main/java/com/hoderick/rabbithole/user/UserProfileMapper.java
+++ src/main/java/com/hoderick/rabbithole/user/UserProfileMapper.java
@@ -1,23 +1,28 @@
 package com.hoderick.rabbithole.user;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+import com.hoderick.rabbithole.s3.S3ClientConfig;
+import com.hoderick.rabbithole.s3.S3StorageService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.net.URL;
+
 @Component
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 class UserProfileMapper {
 
-    private final String avatarBucketBaseUrl;
-
-    UserProfileMapper(@Qualifier("avatarBucketBaseUrl") String avatarBucketBaseUrl) {
-        this.avatarBucketBaseUrl = avatarBucketBaseUrl;
-    }
+    private final S3ClientConfig s3ClientConfig;
+    private final S3StorageService s3StorageService;
 
     UserProfileDto toDto(UserProfile userProfile) {
         String avatarKey = userProfile.getAvatarKey();
-        String avatarUrl = (avatarKey != null) ?
-                avatarBucketBaseUrl + "/" + avatarKey :
-                null;
+        String avatarBucket = s3ClientConfig.getAvatarBucketName();
+        URL presignedAvatarUrl = s3StorageService.generatePresignedUrl(avatarBucket, avatarKey);
 
-        return new UserProfileDto(userProfile.getDisplayName(), userProfile.getEmail(), userProfile.getBio(), avatarUrl);
+        return new UserProfileDto(userProfile.getDisplayName(),
+                userProfile.getEmail(),
+                userProfile.getBio(),
+                presignedAvatarUrl.toString());
     }
 }
